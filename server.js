47a51f64eddd978c45727df3b77afe2f364a15f8
Shashvat Sinha <shashvat@modelis.ai>
2019-03-24 22:25:38 -0400

    Simple express server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 // Express with jwt based access control
+
+const express = require('express')
+const app = express()
+const port = 80
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
