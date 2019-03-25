4631249cde9808a032c6048b826570e244a15c29
Shashvat Sinha <shashvat@modelis.ai>
2019-03-24 23:17:50 -0400

    Some basic routing

--- server.js
+++ server.js
@@ -4,6 +4,29 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+})
+
+app.get('/user', function (req, res) {
+    res.send('Got a GET request at /user')
+})
+
+app.post('/user', function (req, res) {
+    res.send('Got a POST request at /user')
+})
+
+app.put('/user', function (req, res) {
+    res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+    res.send('Got a DELETE request at /user')
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
